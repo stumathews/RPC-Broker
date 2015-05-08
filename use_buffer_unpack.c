6ec05a1cb74b6166e91483c35e730a397f7a9b99
Stuart Mathews <stumathews@gmail.com>
2015-05-08 10:16:57 +0100

    added doc comments#

--- use_buffer_unpack.c
+++ use_buffer_unpack.c
@@ -8,7 +8,8 @@ void prepare(msgpack_sbuffer* sbuf) {
 
     msgpack_packer_init(&pk, sbuf, msgpack_sbuffer_write);
     /* 0st Object */
-
+    
+    // pack a map with 2 items
     msgpack_pack_map(&pk,2);
     msgpack_pack_str(&pk, 4);
     msgpack_pack_str_body(&pk, "Name", 4);
@@ -32,38 +33,58 @@ void prepare(msgpack_sbuffer* sbuf) {
     msgpack_pack_array(&pk, 2);
     msgpack_pack_int(&pk, 42);
     msgpack_pack_false(&pk);
+    msgpack_pack_str(&pk, 7);
+    msgpack_pack_str_body(&pk, "ExAmPlE", 7);
 }
 
 void unpack(char const* buf, size_t len) {
+    
     /* buf is allocated by client. */
     msgpack_unpacked result;
-    size_t off = 0;
     msgpack_unpack_return ret;
+    size_t off = 0;
     int i = 0;
     msgpack_unpacked_init(&result);
+
+    // Go ahead unpack an object
     ret = msgpack_unpack_next(&result, buf, len, &off);
-    while (ret == MSGPACK_UNPACK_SUCCESS) {
+
+    // Go and get the rest of all was good
+    while (ret == MSGPACK_UNPACK_SUCCESS) 
+    {
+        /* Use obj. */
+        printf("Object no %d:\n", ++i);
+        
+        // We have the object
         msgpack_object obj = result.data;
+        // print it:
+
         if( obj.type == MSGPACK_OBJECT_ARRAY )
         {
             //decode the array
-            printf("size of the array is %d\n",obj.via.array.size );
-
+            printf("Array Detected. Size of the array is: %d\n",obj.via.array.size );
+            
+            // array is in formt [int, boolean, string], iterate over it
+            for( int i = 0; i < obj.via.array.size; i++)
+            {
+                printf("Element %d type is: %d\n", i, obj.via.array.ptr[i].type);
+            }
         }
-
-        if( obj.type == MSGPACK_OBJECT_MAP )
+        else if( obj.type == MSGPACK_OBJECT_MAP )
         {
             //decode the map
-
-            printf("size of the map is %d\n",obj.via.map.size );
+            printf("Map Detected. Size of the map is: %d\n", obj.via.map.size );
+            
+            for( int i = 0; i < obj.via.map.size;i++)
+            {
+                printf("Element %d type is: %d\n",i, obj.via.map.ptr[i].val.type);
+            }
         }
 
-
-        printf("type is %d\n", obj.type);
         /* Use obj. */
-        printf("Object no %d:\n", ++i);
+        printf("Preview: ");
         msgpack_object_print(stdout, obj);
-        printf("\n");
+        printf("\n\n");
         /* If you want to allocate something on the zone, you can use zone. */
         /* msgpack_zone* zone = result.zone; */
         /* The lifetime of the obj and the zone,  */
