ad518d148b354dc1c5f4a2d1cb65c2ee672bb1be
Stuart Mathews <stumathews@gmail.com>
2015-05-15 22:21:16 +0100

    send registration buffer over wire. refactor

--- common.c
+++ common.c
@@ -2,8 +2,57 @@
 #include <stulibc.h>
 #include <msgpack.h>
 
+void pack_map_str( char* key, char* value, msgpack_packer* pk);
+void pack_map_int(char* key, int ival,msgpack_packer* pk );
+
+bool service_register_with_broker( char *broker_address, char* broker_port )
+{
+    ServiceReg *sr = Alloc( sizeof( ServiceReg ) );
+    sr->address = "localhost";
+    sr->port = "9090";
+    
+    msgpack_sbuffer sbuf;
+    msgpack_sbuffer_init(&sbuf);
+    msgpack_packer pk;
+    msgpack_packer_init(&pk, &sbuf, msgpack_sbuffer_write);
+
+    pack_map_int("request_type",REQUEST_REGISTRATION,&pk);
+    pack_map_str("sender-address","localhost",&pk);
+    pack_map_str("reply-port","8080",&pk);
+    pack_map_str("service-name","theServiceName",&pk);
+
+    char* services[] = {"service1","service2","server3","service4",NULL };
+    char* service = services[0];
+    int i = 0;
+    while( services[i] != NULL )
+    {
+        PRINT("Service %s.\n", services[i]);
+        i++;
+    }
+    msgpack_pack_map(&pk,1);
+    msgpack_pack_str(&pk, 8);
+    msgpack_pack_str_body(&pk, "services", 8);
+    msgpack_pack_array(&pk, i);
+
+    PRINT("num services %d\n",i);
+    while( i >= 0 )
+    {
+        if( !STR_IsNullOrEmpty(services[i] ))
+        {
+            PRINT("service packed is %s\n", services[i]);
+            msgpack_pack_str(&pk, strlen(services[i]));
+            msgpack_pack_str_body(&pk, services[i], strlen(services[i]));
+        }
+        i--;    
+    }
+    unpack_request_data(sbuf.data, sbuf.size);
+    send_request( sbuf.data, sbuf.size, broker_address, broker_port);
+    msgpack_sbuffer_destroy(&sbuf);
+
+}
+
 // Send request to broker.
-void send_request(char* buffer, int bufsize,char* address, char* port)
+int send_request(char* buffer, int bufsize,char* address, char* port)
 {
     // --------------------------
     // SEND PACKED DATA TO BROKER
@@ -13,10 +62,10 @@ void send_request(char* buffer, int bufsize,char* address, char* port)
 	SOCKET s;
     
 	s = netTcpClient(address,port);
-	client( s, &peer, buffer,bufsize );
+	return client( s, &peer, buffer,bufsize );
 }
 // Send request to listening broker socket
-void client(SOCKET s, struct sockaddr_in* peerp, char* buffer, int length)
+int client(SOCKET s, struct sockaddr_in* peerp, char* buffer, int length)
 {
     struct packet pkt;
     pkt.len = htonl(length);
@@ -42,42 +91,48 @@ void client(SOCKET s, struct sockaddr_in* peerp, char* buffer, int length)
     
     // wait for a response from the broker?
     //unpack_data((const char*)buffer,length);
-    
+    return rc;
 
 }
-// pack the object to send to the broker.
-char* pack_request_data( msgpack_sbuffer* sbuf, char* op,char* fmt, ...)
+
+
+void pack_map_int(char* key, int ival,msgpack_packer* pk )
+{
+    msgpack_pack_map(pk,1);
+    msgpack_pack_str(pk, strlen(key));
+    msgpack_pack_str_body(pk, key, strlen(key));
+    msgpack_pack_int(pk, ival);
+
+}
+// Packs a key/value pair like this: {"key"=>"valuet"}
+// where key is a string and value if an char
+void pack_map_str( char* key, char* value, msgpack_packer* pk)
 {
 
-    /* ********************
-     * Broker Object format:
-     * ********************
-        
-       {"op"=>"getServerDate"}
-       {"params" => [ buffer,length ]}
+    msgpack_pack_map(pk,1);
+    msgpack_pack_str(pk, strlen(key));
+    msgpack_pack_str_body(pk, key, strlen(key));
+    msgpack_pack_str(pk, strlen(value));
+    msgpack_pack_str_body(pk, value, strlen(value));
+}
 
-     */
+// pack the object to send to the broker.
+char* pack_client_request_data( msgpack_sbuffer* sbuf, char* op,char* fmt, ...)
+{
+
     
     // ---------
     // PACK DATA
     // ----------
-    
     
     msgpack_sbuffer_init(sbuf);
     
     msgpack_packer pk;
     msgpack_packer_init(&pk, sbuf, msgpack_sbuffer_write);
     
-    // 0 = Service request
-    msgpack_pack_int(&pk,0);
-    
-    // {"op"=>"getServerDate"}
-    msgpack_pack_map(&pk,1);
-    msgpack_pack_str(&pk, 2);
-    msgpack_pack_str_body(&pk, "op", 2);
-    msgpack_pack_str(&pk, strlen(op));
-    msgpack_pack_str_body(&pk, op, strlen(op));
-    
+    pack_map_int("request_type",0,&pk);
+    pack_map_str("op",op,&pk);
+
     // {"params" => [ {"buffer"=>buffer}, {"length"=>length} ]}}
     msgpack_pack_map(&pk,1);
     msgpack_pack_str(&pk, 6);
@@ -150,6 +205,9 @@ void unpack_request_data(char const* buf, size_t len)
         /* Use obj. */
         msgpack_object_print(stdout, obj);
         printf("\n");
+
+        // Read all the headers
+        // read the specific structures for this request type.
 
         /* If you want to allocate something on the zone, you can use zone. */
         /* msgpack_zone* zone = result.zone; */
--- common.h
+++ common.h
@@ -13,12 +13,11 @@ struct packet {
     char* buffer;
 };
 
-struct ServiceRegistration
+typedef struct ServiceRegistration
 {
     char* address;
     char* port;  
-    char** services; // ["getServername, "GetServerThis", "getServerThat"]
-};
+} ServiceReg;
 
 struct ServiceRegistrationsList
 {
@@ -29,9 +28,10 @@ struct ServiceRegistrationsList
 
 
 enum RequestType determine_request_type(struct packet* pkt);
-void send_request(char* buffer, int bufsize,char* address, char* port);
-void client(SOCKET s, struct sockaddr_in* peerp, char* buffer, int length);
+int send_request(char* buffer, int bufsize,char* address, char* port);
+int client(SOCKET s, struct sockaddr_in* peerp, char* buffer, int length);
 void unpack_request_data(char const* buf, size_t len);
-char* pack_request_data( msgpack_sbuffer* sbuf, char* op,char* fmt, ...);
+char* pack_client_request_data( msgpack_sbuffer* sbuf, char* op,char* fmt, ...);
 void _return();
+bool service_register_with_broker( char* broker_address, char *broker_port );
 #endif
