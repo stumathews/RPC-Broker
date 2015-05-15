c7993c674df14696a2508673a82d54d7293f0df8
Stuart Mathews <stumathews@gmail.com>
2015-05-15 22:22:27 +0100

    added server and server proxy

---
+++ server-proxy.c
@@ -0,0 +1,173 @@
+#include <stulibc.h>
+#include "common.h"
+#include "server_interface.h"
+
+char *program_name;
+static char port[20] = {0};
+static char broker_port[20] = {0};
+static char broker_address[30] = {0};
+static bool waitIndef = false;
+static bool verbose = false;
+static bool registered_with_broker = false;
+
+// read and process data on the socket.
+static void server( SOCKET s, struct sockaddr_in *peerp )
+{
+
+    // Wait for connections from the broker.
+    struct packet pkt;
+    int n_rc = netReadn( s,(char*) &pkt.len, sizeof(uint32_t));
+    pkt.len = ntohl(pkt.len);
+
+    if( verbose) PRINT("received %d bytes and interpreted it as length of %u\n", n_rc,pkt.len );
+    if( n_rc < 1 )
+        netError(1, errno,"failed to receiver packet size\n");
+    
+    char* dbuf = (char*) malloc( sizeof(char) * pkt.len);
+    int d_rc  = netReadn( s, dbuf, sizeof( char) * pkt.len);
+
+    if( d_rc < 1 )
+        netError(1, errno,"failed to receive message\n");
+    
+    if(verbose)
+    {
+        printf("read %d bytes of data\n",d_rc);
+        PRINT("Successfully received buffer:");
+    }
+    unpack_request_data( (const char*)dbuf,pkt.len);
+
+}
+
+static void setBrokerPort( char* arg)
+{
+    CHECK_STRING(arg, IS_NOT_EMPTY);
+    strncpy( broker_port, arg, strlen(arg));
+}
+
+static void setPortNumber(char* arg)
+{
+    CHECK_STRING(arg, IS_NOT_EMPTY);
+    strncpy( port, arg, strlen(arg));
+}
+
+static void setBrokerAddress(char* arg)
+{
+    CHECK_STRING( arg, IS_NOT_EMPTY );
+    strncpy( broker_address, arg, strlen(arg) );
+}
+
+static void setWaitIndef(char* arg)
+{
+    waitIndef = true;
+}
+
+static void setBeVerbose(char* arg)
+{
+    verbose = true;
+}
+
+int main( int argc, char **argv )
+{
+    LIB_Init();
+
+    struct Argument* portNumber = CMD_CreateNewArgument("port","port <number>","Set the port that the server will listen on", true, true, setPortNumber);
+    struct Argument* brokerAddressCMD = CMD_CreateNewArgument("baddress","baddress <address>","Set the address of the broker", true, true, setBrokerAddress);
+    struct Argument* brokerPortCMD = CMD_CreateNewArgument("bport","bport <port>","Set the port of the broker", true, true, setBrokerPort);
+    struct Argument* waitIndefCMD = CMD_CreateNewArgument("waitindef","","wait indefinitely for connections", false, false, setWaitIndef);
+    struct Argument* beVerboseCMD = CMD_CreateNewArgument("verbose","","be verbose wtih messages", false, false, setBeVerbose);
+
+    CMD_AddArgument(portNumber);
+    CMD_AddArgument(waitIndefCMD);
+    CMD_AddArgument(brokerAddressCMD);
+    CMD_AddArgument(brokerPortCMD);
+    CMD_AddArgument(beVerboseCMD);
+
+    struct sockaddr_in local;
+    struct sockaddr_in peer;
+    char *hname;
+    char *sname;
+    int peerlen;
+    SOCKET s1;
+    SOCKET s;
+    fd_set readfds;
+    FD_ZERO( &readfds);
+    const int on = 1;
+
+    struct timeval timeout = {.tv_sec = 60, .tv_usec=0}; 
+
+    if( argc > 1 )
+    {
+        enum ParseResult result = CMD_Parse(argc,argv,true);
+        if( result != PARSE_SUCCESS )
+        {
+            PRINT("There was a problem parsing: %d \n", result);
+            return 1;
+        }
+    }
+    else
+    {
+        CMD_ShowUsages("server <options>");
+        exit(0);
+    }
+
+    INIT();
+    
+    if(  verbose ) PRINT("Server listening...\n");
+    // Rgister with the broker.
+    if( service_register_with_broker( broker_address, broker_port ))
+        registered_with_broker = true;
+
+    // get a socket, bound to this address thats configured to listen.
+    // NB: This is always ever non-blocking 
+    s = netTcpServer("localhost",port);
+
+    FD_SET(s, &readfds);
+    if(verbose) PRINT("About wait for read on port %s...\n", port);
+    do
+    {
+        // wait/block on this listening socket...
+        int res = 0;
+        if( !waitIndef )
+            res = select( s+1, &readfds, NULL, NULL, &timeout);
+        else
+            res = select( s+1, &readfds, NULL, NULL, NULL);
+
+        // wait/block 
+
+        if( res == 0 )
+        {
+            LOG( "timeout");
+            netError(1,errno,"timeout!");
+        }
+        else if( res == -1 )
+        {
+            LOG("Select error!");
+            netError(1,errno,"select error!!");
+        }
+        else
+        {
+            peerlen = sizeof( peer );
+
+            if( FD_ISSET(s,&readfds ))
+            {
+                s1 = accept( s, ( struct sockaddr * )&peer, &peerlen );
+
+                if ( !isvalidsock( s1 ) )
+                    netError( 1, errno, "accept failed" );
+
+                // do network functionality on this socket that now represents a connection with the peer (client) 
+                server( s1, &peer );
+                CLOSE( s1 );
+            }
+            else
+            {
+                DBG("not our socket. continuing");
+                continue;
+            }
+        }
+
+    } while ( 1 );
+
+    LIB_Uninit();
+    EXIT( 0 );
+}
---
+++ server.c
@@ -0,0 +1,21 @@
+#include "server_interface.h"
+#include <string.h>
+#include <stulibc.h>
+
+void getServerDate( char* buffer, int length )
+{
+    char* date = "20 jan 2012";
+    strncpy( buffer, date, strlen(date) <= length ? strlen(date): length);
+}
+
+char* getBrokerName()
+{
+    char* name = Alloc( sizeof(char) );
+    strcpy( name, "peter");
+    return name;
+}
+
+void echo(char* data)
+{
+    STR_Reverse(data);
+}
