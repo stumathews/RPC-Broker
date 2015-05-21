f1a141ca57f17820c7a0b797f4c3f53d3248a680
Stuart Mathews <stumathews@gmail.com>
2015-05-21 15:20:31 +0100

    protocol doc

---
+++ protocol.h
@@ -0,0 +1,33 @@
+
+
+
+/* -----------------------------
+ * Broker Protocol specification
+ * ------------------------------
+
+    {type: iut} 0|1
+    {id: int}
+    {header_n:value_n}
+    {header_n+1:value_n+1}
+    {header...:value...}
+    {sender: ip|dns" }
+
+ServiceRegistration:
+
+    {services: ["service1", "service2","service3"]}
+    {servicename: "ServiceId"}
+
+ServiceRequest:
+
+   {"op"=>"getServerDate"}
+   {"params" => [ buffer,length ]}
+
+ServiceRegistrationReply:
+
+    ack_result: int
+
+ServiceRequestReply:
+
+    { result:[data1, data2] }
+
+*/
