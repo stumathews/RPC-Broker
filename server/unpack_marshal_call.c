36904b19f8ac529c1116f1f209a74ddecbf326d3
Stuart Mathews <stumathews@gmail.com>
2016-03-29 21:23:13 +0100

    refactor

--- server/unpack_marshal_call.c
+++ server/unpack_marshal_call.c
@@ -2,16 +2,10 @@
 #include "common.h"
 #include "server_interface.h"
 
-extern bool verbose;
+
 extern char broker_address[MAX_ADDRESS_CHARS];
 extern char broker_port[MAX_PORT_CHARS];
 void call_marshelResponse_send(int message_id, char* operation, char* broker_address, char* broker_port, bool verbose, char** params);
-
-void copyString(int str_len, const msgpack_object_str* from, char* to) {
-	memset(to, '\0', str_len);
-	to[str_len] = '\0';
-	strncpy(to, from->ptr, str_len);
-}
 
 /**
  * @brief Unpackets service request, extracts parameters and calls server functions and sends back the response to the broker
@@ -20,66 +14,55 @@ void copyString(int str_len, const msgpack_object_str* from, char* to) {
  * @param buflen the length of the data
  * @return void
  */
-void unpack_marshal_call_send(char* buffer, int buflen)
+void unpack_marshal_call_send(char* buffer, int buflen, bool verbose)
 {
     msgpack_unpacked unpacked_result;
     msgpack_unpack_return return_status;
     size_t off = 0;
     int i = 0;
-    msgpack_unpacked_init(&unpacked_result);
     void** params = 0;
     int message_id;
-    return_status = msgpack_unpack_next(&unpacked_result, buffer, buflen, &off);
     
-    while (return_status == MSGPACK_UNPACK_SUCCESS) 
+    msgpack_unpacked_init(&unpacked_result);
+
+    while ((return_status = msgpack_unpack_next(&unpacked_result, buffer, buflen, &off)) == MSGPACK_UNPACK_SUCCESS)
     {
+    	char header_name[MAX_HEADER_NAME_SIZE];
+    	char* ptrOperation;
+
         msgpack_object obj = unpacked_result.data;
-        // stores the protocol header
-        char header_name[MAX_HEADER_NAME_SIZE];
-        // stores the service request operation name
-        char* operation;
         memset(header_name, '\0', MAX_HEADER_NAME_SIZE);
         msgpack_object val = extract_header(&obj, header_name);
 
-        // Extract the operation name to call.
-        if(STR_Equals( "op", header_name) && val.type == MSGPACK_OBJECT_STR) {
-            char* str = (char*) malloc(sizeof(char) * (size_t)val.via.str.size +1);
-            copyString(val.via.str.size, &val.via.str, str);
-            operation = str;
+        if(STR_Equals(OPERATION_HDR, header_name) && val.type == MSGPACK_OBJECT_STR) {
+            char* ptrStr = (char*) malloc(sizeof(char) * (size_t)val.via.str.size +1);
+            copyString(val.via.str.size, &val.via.str, ptrStr);
+            ptrOperation = ptrStr;
         } else if(STR_Equals(MESSAGE_ID_HDR, header_name) && val.type == MSGPACK_OBJECT_POSITIVE_INTEGER) {
-        	//param is an int
             int64_t ival = val.via.i64;
             message_id = ival;
         } else if(val.type == MSGPACK_OBJECT_ARRAY) {
-        	// Get list of parameters for the operation
-            params = malloc( sizeof(void*) * val.via.array.size);
+            params = calloc(val.via.array.size, sizeof(void*));
             for(int i = 0; i < val.via.array.size;i++) {
                 msgpack_object_type param_type = val.via.array.ptr[i].type;
                 msgpack_object param = val.via.array.ptr[i];
-                params[i] = NULL;
                 if(param_type == MSGPACK_OBJECT_STR) {
-                	//param is a char*
-		    int alloc_size = param.via.str.size +1;
-                    char* str = malloc(sizeof(char) * alloc_size );
-                    copyString(val.via.str.size, &val.via.str, str);
-                    params[i] = str;
+                	int alloc_size = param.via.str.size+1;
+                    char* ptrStr = malloc(sizeof(char) * alloc_size);
+                    copyString(val.via.str.size, &val.via.str, ptrStr);
+                    params[i] = ptrStr;
                 } else if(param_type == MSGPACK_OBJECT_POSITIVE_INTEGER) {
-                	//param is an int
-                     int64_t *pival = malloc(sizeof(int));
-                     *pival = param.via.i64;
-                     params[i] = pival;
+                    int64_t *pival = malloc(sizeof(int));
+                    *pival = param.via.i64;
+                    params[i] = pival;
                 }
             }
             // Now arrange for the service call to be invoked and marshal the parmeters into the function call
-            call_marshelResponse_send(message_id, operation, broker_address, broker_port, verbose,(char**)params);
+            call_marshelResponse_send(message_id, ptrOperation, broker_address, broker_port, verbose,(char**)params);
         }
-        return_status = msgpack_unpack_next(&unpacked_result, buffer, buflen, &off);
     } // finished unpacking.
-
-    msgpack_unpacked_destroy(&unpacked_result);
-
-    if (return_status == MSGPACK_UNPACK_PARSE_ERROR) 
-    {
+    if (return_status == MSGPACK_UNPACK_PARSE_ERROR) {
         PRINT("The data in the buf is invalid format.\n");
     }
+    msgpack_unpacked_destroy(&unpacked_result);
 }
