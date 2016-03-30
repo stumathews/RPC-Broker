99e2a6a8f9c9b8e822830287ffe423b4cfbab88f
Stuart Mathews <stuart@stuartmathews.com>
2016-03-30 10:46:48 +0100

    tidy up

--- broker/unpack_service_registration_buffer.c
+++ broker/unpack_service_registration_buffer.c
@@ -1,42 +1,39 @@
 #include "broker_support.h"
 #include "common.h"
 
-extern char port[MAX_PORT_CHARS];
-extern bool verbose;
-extern struct ServiceRegistration service_repository;
-
-struct ServiceRegistration* unpack_service_registration_buffer(const char* buffer, int buflen, struct BrokerConfig* brokerConfig)
+struct ServiceRegistration* unpack_service_registration_buffer(const char* payload, int langth, struct BrokerConfig* brokerConfig)
 {
     printf("Unpacking service registration request...\n");
-
-    struct ServiceRegistration* unpacked = malloc(sizeof(struct ServiceRegistration));
-    unpacked->num_services = 0;
 
     size_t off = 0;
     int i = 0;
     msgpack_unpacked unpacked_result;
     msgpack_unpack_return return_status;
+
+    struct ServiceRegistration* unpacked = malloc(sizeof(struct ServiceRegistration));
+	unpacked->num_services = 0;
+
     msgpack_unpacked_init(&unpacked_result);
 
-    return_status = msgpack_unpack_next(&unpacked_result, buffer, buflen, &off);
-
-    while (return_status == MSGPACK_UNPACK_SUCCESS) 
+    while ((return_status = msgpack_unpack_next(&unpacked_result, payload, langth, &off)) == MSGPACK_UNPACK_SUCCESS)
     {
-        msgpack_object obj = unpacked_result.data;
-
+        msgpack_object obj;
+        msgpack_object val;
         char header_name[MAX_HEADER_NAME_SIZE];
         memset(header_name, '\0', MAX_HEADER_NAME_SIZE);
 
-        msgpack_object val = extract_header( &obj, header_name);
+        obj = unpacked_result.data;
+        val = extract_header( &obj, header_name);
 
         if(val.type == MSGPACK_OBJECT_STR)
         {
-            int str_len = val.via.str.size;
-            char* str = (char*) malloc(sizeof(char) * (size_t)str_len);
+            int str_len;
+            char* str;
 
-            memset(str, '\0', str_len);
-            str[str_len] = '\0';
-            strncpy(str, val.via.str.ptr, str_len);
+            str_len= val.via.str.size;
+            str = (char*) malloc(sizeof(char) * (size_t)str_len);
+
+            copyString(str_len, &val.via.str, str);
 
             if(STR_Equals(SENDER_ADDRESS_HDR, header_name) == true) {
                 unpacked->address = str;
@@ -50,7 +47,7 @@ struct ServiceRegistration* unpack_service_registration_buffer(const char* buffe
         }
         else if(val.type == MSGPACK_OBJECT_POSITIVE_INTEGER)
         {
-            if( STR_Equals(SERVICES_COUNT_HDR, header_name) == true) {
+            if(STR_Equals(SERVICES_COUNT_HDR, header_name) == true) {
                 unpacked->num_services = val.via.i64;
                 unpacked->services = malloc((sizeof(char*) * (val.via.i64) + val.via.i64));
             }
@@ -62,41 +59,35 @@ struct ServiceRegistration* unpack_service_registration_buffer(const char* buffe
 
             for( int i = 0; i < array.size; i++)
             {
-                struct msgpack_object curr = array.ptr[i];
-                int str_len = curr.via.str.size;
-                char* str = (char*) malloc((size_t)str_len+1);
-                if(!str){
+                struct msgpack_object curr;
+                int str_len;
+                char* str;
+
+                curr = array.ptr[i];
+                str_len = curr.via.str.size;
+                str = (char*) malloc((size_t)str_len+1);
+
+                if(!str) {
                 	printf("failed to malloc!\n");
                 	printf("curr.via.str.size %u \n",(int)curr.via.str.size);
                 	perror("");
                 	return 0;
-
                 }
 
-                memset( str, '\0', str_len);
-                str[str_len] = '\0';
-                strncpy(str, curr.via.str.ptr, str_len);
+                copyString(str_len, &curr.via.str, str);
                 unpacked->services[i] = str;
 
-
                 if(brokerConfig->verbose) {
-
                     PRINT("Found service: '%s'\n", str);
                 }
             }
-
         } //array processing end
-        else
-        {
-            // this is not a header or array but something else
-        }
-        return_status = msgpack_unpack_next(&unpacked_result, buffer, buflen, &off);
+        else  { /* this is not a header or array but something else */ }
     } // finished unpacking.
 
     msgpack_unpacked_destroy(&unpacked_result);
 
-    if (return_status == MSGPACK_UNPACK_PARSE_ERROR) 
-    {
+    if (return_status == MSGPACK_UNPACK_PARSE_ERROR) {
         PRINT("The data in the buf is invalid format.\n");
     }
     return unpacked;
