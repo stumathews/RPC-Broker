4c9c738b559707b58556f1238c055d90bcf70b15
Stuart Mathews <stuart@stuartmathews.com>
2016-04-15 17:29:36 +0100

    cleanup forward_response_to_client

--- broker/forward_response_to_client.c
+++ broker/forward_response_to_client.c
@@ -2,15 +2,10 @@
 #include "broker_support.h"
 #include <unistd.h>
 
-void forward_response_to_client(Packet* response, struct Config *brokerConfig) {
+void forward_response_to_client(Packet* response, struct Config *brokerConfig)
+{
 	Location* client = malloc(sizeof(Location));
-
 	find_client_for_response(response, client, brokerConfig);
 	PRINT(">>> Forward response to client\n");
-
 	send_request(response, client->address, client->port, false);
-
-	//free(client->port);
-	//free(client->address);
-	//free(client);
 }
