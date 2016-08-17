5117f90def044fa36920f5ff07377a0cf7ccc1b6
Stuart Mathews <stumathews@gmail.com>
2016-08-17 20:52:17 +0100

    tidy up

--- client/send_and_receive.c
+++ client/send_and_receive.c
@@ -6,8 +6,8 @@ extern bool wait_response_indef;
 extern char client_address[MAX_ADDRESS_CHARS];
 static Packet *get_response(SOCKET s, struct sockaddr_in *peerp, bool verbose);
 
-Packet* waitForResponse(_Bool verbose, char client_address[MAX_ADDRESS_CHARS],
-		_Bool wait_response_indef, char* wait_response_port) {
+Packet* waitForResponse(_Bool verbose, char client_address[MAX_ADDRESS_CHARS], _Bool wait_response_indef, char* wait_response_port)
+{
 	struct sockaddr_in local;
 	struct sockaddr_in peer;
 	char* hname;
@@ -19,47 +19,46 @@ Packet* waitForResponse(_Bool verbose, char client_address[MAX_ADDRESS_CHARS],
 	FD_ZERO(&readfds);
 	const int on = 1;
 	Packet* result;
-struct timeval timeout = {.tv_sec = 60, .tv_usec = 0}
-;
-NETINIT();
-if (verbose) {
-	PRINT("client waiting for response...\n");
-}
-s = netTcpServer(client_address, wait_response_port);
-FD_SET(s, &readfds);
-if (verbose) {
-	PRINT("About wait for read on port %s...\n", wait_response_port);
-}
-// wait/block on this listening socket...
-int res = 0;
-if (!wait_response_indef) {
-	res = select(s + 1, &readfds, NULL, NULL, &timeout);
-} else {
-	res = select(s + 1, &readfds, NULL, NULL, NULL);
-}
-if (res == 0) {
-	LOG("timeout");
-	netError(1, errno, "timeout!");
-} else if (res == -1) {
-	LOG("Select error!");
-	netError(1, errno, "select error!!");
-} else {
-	peerlen = sizeof(peer);
-	if (FD_ISSET(s, &readfds)) {
-		s1 = accept(s, (struct sockaddr*) &peer, &peerlen);
-		if (!isvalidsock(s1)) {
-			netError(1, errno, "accept failed");
-		}
-		// do network functionality on this socket that now represents a connection with the peer (client)
-		result = get_response(s1, &peer, verbose);
-		NETCLOSE(s1);
-	} else {
-		DBG("not our socket. continuing");
+	struct timeval timeout = {.tv_sec = 60, .tv_usec = 0};
+	NETINIT();
+	if (verbose) {
+		PRINT("client waiting for response...\n");
 	}
-}
+	s = netTcpServer(client_address, wait_response_port);
+	FD_SET(s, &readfds);
+	if (verbose) {
+		PRINT("About wait for read on port %s...\n", wait_response_port);
+	}
+	// wait/block on this listening socket...
+	int res = 0;
+	if (!wait_response_indef) {
+		res = select(s + 1, &readfds, NULL, NULL, &timeout);
+	} else {
+		res = select(s + 1, &readfds, NULL, NULL, NULL);
+	}
+	if (res == 0) {
+		LOG("timeout");
+		netError(1, errno, "timeout!");
+	} else if (res == -1) {
+		LOG("Select error!");
+		netError(1, errno, "select error!!");
+	} else {
+		peerlen = sizeof(peer);
+		if (FD_ISSET(s, &readfds)) {
+			s1 = accept(s, (struct sockaddr*) &peer, &peerlen);
+			if (!isvalidsock(s1)) {
+				netError(1, errno, "accept failed");
+			}
+			// do network functionality on this socket that now represents a connection with the peer (client)
+			result = get_response(s1, &peer, verbose);
+			NETCLOSE(s1);
+		} else {
+			DBG("not our socket. continuing");
+		}
+	}
 
-NETCLOSE (s);
-return result;
+	NETCLOSE (s);
+	return result;
 }
 
 /**
